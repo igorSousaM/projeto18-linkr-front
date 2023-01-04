57cfa9f42d9110cdd6b12c6a771331cc45bc6423
GiovanneAndrade <vanne.as@hotmail.com>
2023-01-04 17:03:13 -0300

    feat:  context

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
-
+import './css/reset.css'
+import { AuthProvider } from './providers/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
 );
 
